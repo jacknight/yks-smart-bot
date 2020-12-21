996f58d4344fb01b76f1005c3ba4ab36e857a623
jacknight <jacknight@gmail.com>
2020-12-21 22:47:25 +0200

    feat: :rocket: Change socket connect

--- client.js
+++ client.js
@@ -1,5 +1,5 @@
 (function connect() {
-  let socket = io.connect(`http://localhost:3000`);
+  let socket = io.connect(`https://discord-buzzer.herokuapp.com`);
 
   const modeP = document.querySelector("#mode");
   const modeButton = document.querySelector("button[name='mode']");
