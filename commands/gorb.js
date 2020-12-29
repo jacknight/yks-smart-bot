2c8072980739d6045581084d95cafa3d2267d60f
jacknight <jacknight@gmail.com>
2020-12-29 10:15:49 +0200

    feat(command): :sparkles: Add gorb command (once per hour)

---
+++ commands/gorb.js
@@ -0,0 +1,24 @@
+const { Command } = require("discord-akairo");
+
+class GorbCommand extends Command {
+  constructor() {
+    super("gorb", {
+      cooldown: 3600000,
+      ratelimit: 1,
+      regex: /^gorb$/i,
+    });
+  }
+
+  exec(message) {
+    const self = this;
+    if (!this.client.gorb) {
+      this.client.gorb = true;
+      setTimeout(function () {
+        self.client.gorb = false;
+      }, 3600000);
+      message.channel.send("gorb");
+    }
+  }
+}
+
+module.exports = GorbCommand;
