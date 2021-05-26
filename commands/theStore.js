e8c4b5105347d703f63f6d01eb9131659f6f60f1
jacknight <jacknight@gmail.com>
2021-05-26 15:07:41 +0300

    The return of the store.

---
+++ commands/theStore.js
@@ -0,0 +1,23 @@
+const { Command } = require("discord-akairo");
+
+class TheStoreCommand extends Command {
+  constructor() {
+    super("thestore", {
+      regex: /\b(a|an|the)\b [A-Za-z ]*\bstore\b/i,
+      category: "easter-egg",
+    });
+  }
+
+  exec(message) {
+    try {
+      message.react(
+        this.client.util.resolveEmoji("thestore", message.guild.emojis.cache)
+      );
+    } catch {
+      console.log;
+    }
+    return;
+  }
+}
+
+module.exports = TheStoreCommand;
