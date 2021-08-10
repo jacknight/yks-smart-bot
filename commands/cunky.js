d69fc93f59b25a3e44773a47222eeda8121777f0
jacknight <jacknight@gmail.com>
2021-08-10 17:08:58 +0300

    Cunky

---
+++ commands/cunky.js
@@ -0,0 +1,29 @@
+const { Command } = require("discord-akairo");
+
+class CunkyCommand extends Command {
+  constructor() {
+    super("cunky", {
+      cooldown: 3600000,
+      ratelimit: 1,
+      regex: /^cunky$/i,
+      category: "eater-egg",
+    });
+  }
+
+  exec(message) {
+    if (!this.client.globalRates.get(message.guild.id)) {
+      this.client.globalRates.set(message.guild.id, new Set());
+    }
+
+    if (!this.client.globalRates.get(message.guild.id).has("cunky")) {
+      this.client.globalRates.get(message.guild.id).add("cunky");
+      const self = this;
+      setTimeout(function () {
+        self.client.globalRates.get(message.guild.id).delete("cunky");
+      }, 1000 * 60 * 60 * 12); // twice a day.
+      message.channel.send("cunky");
+    }
+  }
+}
+
+module.exports = CunkyCommand;
