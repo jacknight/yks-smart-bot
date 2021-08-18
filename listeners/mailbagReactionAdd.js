79beb6a66790596468564e2471f7e54041a08766
jacknight <jacknight@gmail.com>
2021-08-18 21:23:55 +0300

    Track :this: reactions to mailbag messages.

---
+++ listeners/mailbagReactionAdd.js
@@ -0,0 +1,71 @@
+const { Listener } = require("discord-akairo");
+
+class MailbagReactionAddListener extends Listener {
+  constructor() {
+    super("mailbagReactionAdd", {
+      emitter: "client",
+      event: "messageReactionAdd",
+    });
+  }
+
+  async exec(reaction, user) {
+    if (reaction.partial) {
+      // try to fetch the message, which may or may not work with a partial
+      try {
+        await reaction.fetch();
+      } catch (error) {
+        console.log("Unable to fetch message for partial reaction:", error);
+        return;
+      }
+    }
+
+    // if the reaction was a ":this:" reaction, and to:
+    // 1. a bot message; and
+    // 2. in the mailbag channel; and
+    // 3. the message has an embed
+    // then update the tally for the message ID in the database
+    if (
+      reaction._emoji.name === "this" &&
+      reaction.message.channel.id === process.env.YKS_MAILBAG_CHANNEL_ID
+    ) {
+      // Get array of existing mailbag messages that have been reacted to
+      const mailbagMessages = await this.client.settings.get(
+        reaction.message.guild.id,
+        "mailbagMessages",
+        []
+      );
+
+      // Check if this message is in the list and if so, grab it and update
+      // the tally. Otherwise, create a new object and add it to the list.
+      let index = JSON.parse(
+        mailbagMessages.findIndex(
+          (mailbagMessage) =>
+            JSON.parse(mailbagMessage).id === reaction.message.id
+        )
+      );
+
+      if (index < 0) {
+        // Create a new object, then stringify it and add it to the array.
+        const msgObject = { id: reaction.message.id, count: 1 };
+        mailbagMessages.push(JSON.stringify(msgObject));
+      } else {
+        // Update the existing object by incrementing the count.
+        const msgObject = JSON.parse(mailbagMessages[index]);
+        msgObject.count++;
+        mailbagMessages[index] = JSON.stringify(msgObject);
+      }
+
+      // Sort, descending.
+      mailbagMessages.sort((a, b) => a.count - b.count);
+
+      // Update database.
+      this.client.settings.set(
+        reaction.message.guild.id,
+        "mailbagMessages",
+        mailbagMessages
+      );
+    }
+  }
+}
+
+module.exports = MailbagReactionAddListener;
---
+++ listeners/mailbagReactionRemove.js
@@ -0,0 +1,72 @@
+const { Listener } = require("discord-akairo");
+
+class MailbagReactionRemoveListener extends Listener {
+  constructor() {
+    super("mailbagReactionRemove", {
+      emitter: "client",
+      event: "messageReactionRemove",
+    });
+  }
+
+  async exec(reaction, user) {
+    if (reaction.partial) {
+      // try to fetch the message, which may or may not work with a partial
+      try {
+        await reaction.fetch();
+      } catch (error) {
+        console.log("Unable to fetch message for partial reaction:", error);
+        return;
+      }
+    }
+
+    // if the reaction was a ":this:" reaction, and to:
+    // 1. a bot message; and
+    // 2. in the kickstarter-bot channel; and
+    // 3. the message has an embed
+    // then update the tally for the message ID in the database
+    if (
+      reaction._emoji.name === "this" &&
+      reaction.message.channel.id === process.env.YKS_MAILBAG_CHANNEL_ID
+    ) {
+      // Get array of existing mailbagMessages that have been reacted to
+      const mailbagMessages = await this.client.settings.get(
+        reaction.message.guild.id,
+        "mailbagMessages",
+        []
+      );
+
+      // Check if this message is in the list and if so, grab it and update
+      // the tally. Otherwise, create a new object and add it to the list.
+      let index = JSON.parse(
+        mailbagMessages.findIndex(
+          (mailbagMessage) =>
+            JSON.parse(mailbagMessage).id === reaction.message.id
+        )
+      );
+
+      if (index >= 0) {
+        // Update the existing object by decrementing the count (or removing
+        // it entirely)
+        const msgObject = JSON.parse(mailbagMessages[index]);
+        msgObject.count--;
+        if (msgObject.count === 0) {
+          mailbagMessages.splice(index, 1);
+        } else {
+          mailbagMessages[index] = JSON.stringify(msgObject);
+        }
+      }
+
+      // Sort, descending.
+      mailbagMessages.sort((a, b) => a.count - b.count);
+
+      // Update database.
+      this.client.settings.set(
+        reaction.message.guild.id,
+        "mailbagMessages",
+        mailbagMessages
+      );
+    }
+  }
+}
+
+module.exports = MailbagReactionRemoveListener;
