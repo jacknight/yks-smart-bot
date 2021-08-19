94dbe7c511b716d8c96b4d4be131d4482714a939
jacknight <jacknight@gmail.com>
2021-08-19 16:54:44 +0300

    Manual welcome for testing/emergencies.

---
+++ commands/manualWelcome.js
@@ -0,0 +1,28 @@
+const { Command } = require("discord-akairo");
+
+class WelcomeCommand extends Command {
+  constructor() {
+    super("newmember", {
+      aliases: ["newmember"],
+      args: [{ id: "id" }],
+      ownerOnly: true,
+    });
+  }
+
+  exec(message, { id }) {
+    // Lookup guild by ID
+    const guildObj = this.client.util.resolveGuild(
+      process.env.YKS_GUILD_ID,
+      this.client.guilds.cache
+    );
+    if (!guildObj) return;
+
+    // Lookup member by ID
+    const member = this.client.util.resolveMember(id, guildObj.members.cache);
+    if (!member) return;
+
+    return this.client.emit("guildMemberAdd", member);
+  }
+}
+
+module.exports = WelcomeCommand;
