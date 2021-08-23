9b3ff8e2daad8e48c0f3a1e9957b7316c8818d9b
jacknight <jacknight@gmail.com>
2021-08-23 11:11:03 +0300

    Remove debugging and extra welcome msg.

--- listeners/memberWelcome.js
+++ listeners/memberWelcome.js
@@ -19,7 +19,7 @@ class MemberWelcomeListener extends Listener {
       "welcomedMembers",
       []
     );
-    // if (welcomedMembers.some((id) => id === member.id)) return;
+    if (welcomedMembers.some((id) => id === member.id)) return;
 
     // Add to welcomed members for guild so we don't do this again.
     welcomedMembers.push(member.id);
@@ -69,30 +69,6 @@ class MemberWelcomeListener extends Listener {
       "welcome-image.png"
     );
 
-    if (member.id === "141822351321989120") {
-      // gorb
-      member.guild.systemChannel.send("gorb", attachment);
-    } else if (member.id === "251217007045902348") {
-      // tay
-      member.guild.systemChannel.send("Tay is back!", attachment);
-    } else {
-      const responses = [
-        "You don't have to be insane to post here, but it's a \"good to have.\"",
-        "I give it a month.",
-        "Make yourself at home.\nOh ok, you're going straight for the nasty channel. Ah! Well. Nevertheless,",
-        "It's not too late to just turn around and walk away. No one would blame you.",
-        "Grab an empty chair in the circle. We're just about to start sharing how YKS ruined our lives.",
-        "If you need to know what episode something happened in, ask vinny.",
-        "If you see JF or DB in here, avert your eyes from their posts as a sign of respect.",
-        "Vote for your favorite episode # using the command `!best <episode number>`",
-      ];
-      member.guild.systemChannel.send({
-        content: `Welcome, ${member}! ${
-          responses[Math.floor(Math.random() * responses.length)]
-        }`,
-        files: [attachment],
-      });
-    }
     const responses = [
       "You don't have to be insane to post here, but it's a \"good to have.\"",
       "I give it a month.",
@@ -101,14 +77,13 @@ class MemberWelcomeListener extends Listener {
       "Grab an empty chair in the circle. We're just about to start sharing how YKS ruined our lives.",
       "If you need to know what episode something happened in, ask vinny.",
       "If you see JF or DB in here, avert your eyes from their posts as a sign of respect.",
-      "Vote for your favorite episode # using the command\n`!best <episode number>`",
-      'JF has blessed your timeline. say "thank you mr. jf" for good fortune in the new year',
+      "Vote for your favorite episode # using the command `!best <episode number>`",
     ];
     member.guild.systemChannel.send({
       content: `Welcome, ${member}! ${
         responses[Math.floor(Math.random() * responses.length)]
       }`,
-      files: ["./assets/jf-blessing.png"],
+      files: [attachment],
     });
   }
 }
