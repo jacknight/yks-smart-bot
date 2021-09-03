0822b3e64698592082f9a98a0a1de8f9920ed76e
jacknight <jacknight@gmail.com>
2021-09-03 15:36:38 +0300

    500th clip surprise.

--- listeners/clip.js
+++ listeners/clip.js
@@ -33,10 +33,39 @@ class ClipListener extends Listener {
             "clips",
             []
           );
-          if (!clips.find((clip) => clip === attachment.url)) {
-            clips.push(attachment.url);
-            this.client.settings.set(message.guild.id, "clips", clips);
+          if (clips.find((clip) => clip === attachment.url)) return;
+
+          if (clips.length === 499) {
+            const mike = this.client.util.resolveMember(
+              "882331792798003250",
+              message.guild.members.cache
+            );
+            const jf = this.client.util.resolveMember(
+              "883216303236722718",
+              message.guild.members.cache
+            );
+            const embed = {
+              color: 0xffc0cb,
+              title: `Congratulations!`,
+              thumbnail: {
+                url: "https://pbs.twimg.com/media/E0-Qc05XsAcTf3T?format=jpg&name=large",
+              },
+              url: `https://pisscord.site`,
+              description: `${message.author} just posted the 500th clip! Please join me in thanking them for helping us reach this milestone and enriching the community in the process!
+
+As a thank you for fully embracing the dirtbag left culture cultivated here in the YKS pisscord, please contact ${mike} or ${jf} for your prize!`,
+              image: {
+                url: "https://cdn.discordapp.com/attachments/672146620275621918/882070930107551774/Screenshot_2021-08-29_22.50.38.png",
+              },
+              footer: {
+                text: "Restrictions apply. Must be 18 or older, but not too old (let's say up to 33 just to pick a number). Must reside in the United States or somewhere cool (please provide proof of this).\n\nThis message is fully approved by Jesse Farrar and Michael Hale and I definitely asked them.",
+              },
+            };
+            message.reply({ embeds: [embed] });
           }
+
+          clips.push(attachment.url);
+          this.client.settings.set(message.guild.id, "clips", clips);
         }
       });
     }
