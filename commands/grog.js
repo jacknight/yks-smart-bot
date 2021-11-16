6da613f8669891ce7d206591ff2e32c196d5fd22
jacknight <jacknight@gmail.com>
2021-11-16 12:49:44 +0200

    grog

---
+++ commands/grog.js
@@ -0,0 +1,29 @@
+const { Command } = require("discord-akairo");
+
+class GrogCommand extends Command {
+  constructor() {
+    super("grog", {
+      cooldown: 3600000,
+      ratelimit: 1,
+      regex: /^grog$/i,
+      category: "eater-egg",
+    });
+  }
+
+  exec(message) {
+    if (!this.client.globalRates.get(message.guild.id)) {
+      this.client.globalRates.set(message.guild.id, new Set());
+    }
+
+    if (!this.client.globalRates.get(message.guild.id).has("grog")) {
+      this.client.globalRates.get(message.guild.id).add("grog");
+      const self = this;
+      setTimeout(function () {
+        self.client.globalRates.get(message.guild.id).delete("grog");
+      }, 1000 * 60 * 60 * 12); // twice a day.
+      message.channel.send("grog");
+    }
+  }
+}
+
+module.exports = GrogCommand;
