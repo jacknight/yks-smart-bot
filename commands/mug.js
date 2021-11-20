edba517af13dc1a8944bb04222f3506dac469642
jacknight <jacknight@gmail.com>
2021-11-20 16:27:09 +0200

    Mug

---
+++ commands/mug.js
@@ -0,0 +1,30 @@
+const { Command } = require("discord-akairo");
+
+class MugCommand extends Command {
+  constructor() {
+    super("mug", {
+      aliases: ["mug", "burgymug"],
+      cooldown: 1000 * 60, // once per min
+      ratelimit: 1,
+    });
+  }
+
+  exec(message) {
+    if (!this.client.globalRates.get(message.guild.id)) {
+      this.client.globalRates.set(message.guild.id, new Set());
+    }
+
+    if (!this.client.globalRates.get(message.guild.id).has("mug")) {
+      this.client.globalRates.get(message.guild.id).add("mug");
+      const self = this;
+      setTimeout(function () {
+        self.client.globalRates.get(message.guild.id).delete("mug");
+      }, 1000 * 60); // once per min
+
+      const attachment = this.client.util.attachment("./assets/mug.png");
+      message.channel.send({ files: [attachment] });
+    }
+  }
+}
+
+module.exports = MugCommand;
