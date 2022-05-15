c7b41a094ab6f67d0acb5d5d1b0f50d26e8210ec
jacknight <jacknight@gmail.com>
2022-05-15 09:23:41 +0300

    !today command, sorta tested.

---
+++ commands/today.js
@@ -0,0 +1,92 @@
+const { Command } = require("discord-akairo");
+
+const Parser = require("rss-parser");
+const parser = new Parser();
+const MAIN_FEED_RSS = process.env.MAIN_FEED_RSS;
+const BONUS_FEED_RSS = process.env.BONUS_FEED_RSS;
+
+const epEmbed = (feedTitle, iconURL, epURL, epTitle, desc) => {
+  console.log(feedTitle, iconURL, epURL, epTitle, desc);
+  const isBonus = epURL.includes("patreon.com");
+  return {
+    color: isBonus ? 0xddaf74 : 0x83c133,
+    title: feedTitle,
+    author: {
+      icon_url: isBonus ? "https://i.imgur.com/5sHYjAX.jpeg" : iconURL,
+      url: "https://www.patreon.com/yourkickstartersucks",
+    },
+    thumbnail: {
+      url: isBonus ? "https://i.imgur.com/5sHYjAX.jpeg" : iconURL,
+    },
+    fields: [
+      {
+        name: epTitle,
+        value: desc,
+        inline: false,
+      },
+      {
+        name: "Link",
+        value: `[Patreon](${epURL})`,
+        inline: false,
+      },
+    ],
+  };
+};
+
+class TodayCommand extends Command {
+  constructor() {
+    super("today", {
+      aliases: ["today"],
+      cooldown: 1000 * 60, // one per min
+      rateLimit: 1,
+    });
+  }
+
+  async exec(message) {
+    const today = new Date().toLocaleString("en-us", {
+      month: "numeric",
+      day: "numeric",
+    });
+
+    const mainFeed = await parser.parseURL(MAIN_FEED_RSS);
+    const bonusFeed = await parser.parseURL(BONUS_FEED_RSS);
+    mainFeed.items = mainFeed.items.map((item) => {
+      return { ...item, feedTitle: mainFeed.title };
+    });
+    bonusFeed.items = bonusFeed.items.map((item) => {
+      return { ...item, feedTitle: bonusFeed.title };
+    });
+    const combinedFeed = mainFeed.items.concat(bonusFeed.items);
+    const filteredFeed = combinedFeed.filter(
+      (episode) =>
+        new Date(episode.isoDate).toLocaleString("en-us", {
+          month: "numeric",
+          day: "numeric",
+        }) === today
+    );
+
+    if (filteredFeed.length > 0) {
+      let embeds = [];
+      filteredFeed.forEach((episode) => {
+        const dateStr = new Date(episode.isoDate).toLocaleString("en-us", {
+          dateStyle: "medium",
+        });
+        embeds.push(
+          epEmbed(
+            dateStr,
+            episode.itunes.image,
+            episode.link,
+            episode.title,
+            episode.contentSnippet
+          )
+        );
+      });
+
+      message.channel.send({ embeds });
+    } else {
+      message.channel.send("Nothing happened on this date.");
+    }
+  }
+}
+
+module.exports = TodayCommand;
