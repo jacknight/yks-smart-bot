078c4b63dfadbaaa93a4307241b0a800a251a814
jacknight <jacknight@gmail.com>
2022-10-13 20:48:24 +0300

    Another day another mug.

--- commands/mug.js
+++ commands/mug.js
@@ -1,11 +1,11 @@
-const { Command } = require("discord-akairo");
-const { random } = require("lodash");
+const { Command } = require('discord-akairo');
+const { random } = require('lodash');
 
 const globalToday = new Date();
 class MugCommand extends Command {
   constructor() {
-    super("mug", {
-      aliases: ["mug", "burgymug"],
+    super('mug', {
+      aliases: ['mug', 'burgymug'],
       ...((globalToday.getDate() !== 7 || globalToday.getMonth() !== 3) && {
         cooldown: 1000 * 60,
         rateLimit: 1,
@@ -18,14 +18,14 @@ class MugCommand extends Command {
       this.client.globalRates.set(message.guild.id, new Set());
     }
 
-    if (!this.client.globalRates.get(message.guild.id).has("mug")) {
-      this.client.globalRates.get(message.guild.id).add("mug");
+    if (!this.client.globalRates.get(message.guild.id).has('mug')) {
+      this.client.globalRates.get(message.guild.id).add('mug');
       const self = this;
       setTimeout(function () {
-        self.client.globalRates.get(message.guild.id).delete("mug");
+        self.client.globalRates.get(message.guild.id).delete('mug');
       }, 1000 * 60); // once per min
 
-      const mugs = ["./assets/mug.png", "./assets/mug2.jpg"];
+      const mugs = ['./assets/mug.png', './assets/mug2.jpg', './assets/mug3.png'];
       const mug = mugs[random(1)];
       const attachment = this.client.util.attachment(mug);
       message.channel.send({ files: [attachment] });
