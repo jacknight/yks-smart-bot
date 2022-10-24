be5375281c83e207968c8c8b4606520fde3940bc
jacknight <jacknight@gmail.com>
2022-10-24 08:51:24 -0400

    Debugging logs.

--- commands/clip.js
+++ commands/clip.js
@@ -13,11 +13,13 @@ class ClipCommand extends Command {
   async checkLink(url) {
     const result = { valid: false, remove: false };
     try {
-      const resp = await axios.get(url.replace('cdn.discordapp.com', 'media.discordapp.net'));
+      console.debug('1. Checking URL:', url);
+      await axios.get(url.replace('cdn.discordapp.com', 'media.discordapp.net'));
+      console.debug("2. Successfully GET'ed URL.");
       result.valid = true;
       result.remove = false;
     } catch (e) {
-      console.error(e);
+      console.error('Failed to GET URL: ', e);
       if (e?.response?.status === 404) {
         console.error('Remvoing url from clips: ', url);
         result.remove = true;
@@ -29,6 +31,7 @@ class ClipCommand extends Command {
 
   async exec(message) {
     const clips = this.client.settings.get(message.guild.id, 'clips', []);
+    console.debug('clips - clips length: ', clips.length);
     if (clips.length > 0) {
       let content = '';
       let index = Math.floor(Math.random() * clips.length);
@@ -44,11 +47,12 @@ class ClipCommand extends Command {
 
       let foundValidLink = false;
       while (!foundValidLink && clips.length > 0) {
+        console.debug('clips - Checking valid link for clips index: ', index);
         const url = clips[index];
         const result = await this.checkLink(url);
         foundValidLink = result.valid;
 
-        // Remove from clips if it through a 404 (someone deleted the post)
+        // Remove from clips if it threw a 404 (someone deleted the post)
         if (result.remove) {
           const loc = clips.indexOf(url);
           if (loc >= 0) {
@@ -60,15 +64,18 @@ class ClipCommand extends Command {
         // If invalid, find a new random index to poke.
         if (!foundValidLink) {
           index = Math.floor(Math.random() * clips.length);
+          console.debug('clips- set new random index and trying again: ', index);
         }
       }
 
       if (content != '') {
+        console.debug('clips - Sending message w/ content: ', content);
         message.channel.send({
           content,
           files: [clips[index]],
         });
       } else {
+        console.debug('clips - Sending message w/o content.');
         message.channel.send({ files: [clips[index]] });
       }
     }
