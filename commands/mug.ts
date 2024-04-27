232e20d2bbe265e4d22e32356a06df6d0e22032b
jacknight <jacknight@gmail.com>
2024-04-27 18:14:17 +0300

    Ok. __dirname?

--- commands/mug.ts
+++ commands/mug.ts
@@ -28,7 +28,11 @@ class MugCommand extends Command {
         self.client.globalRates.get(message.guild!.id).delete('mug');
       }, 1000 * 60); // once per min
 
-      const mugs = ['${HOME}/assets/mug.png', '${HOME}/assets/mug2.jpg', '${HOME}/assets/mug3.png'];
+      const mugs = [
+        `${__dirname}/assets/mug.png`,
+        `${__dirname}/assets/mug2.jpg`,
+        `${__dirname}/assets/mug3.png`,
+      ];
       const mug = mugs[random(2)];
       const attachment = this.client.util.attachment(mug);
       message.channel.send({ files: [attachment] });
